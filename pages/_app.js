9af868ff6fff87615c0808ec23977fa29cf37607
SHANbic <supagoof924@hotmail.com>
2021-12-23 20:46:54 +0100

    added google analytics script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import Layout from "../components/Layout";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script
+        async
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`,
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   );
